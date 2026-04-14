9bb1ac28cc1d6e7a923603a175be2efed012a848
Azadron228 <azamat.ibrayv@gmail.com>
2026-04-14 11:29:56 +0500

    feat: add alembic migration for users table

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
@@ -19,7 +20,9 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+sys.path.append(os.getcwd())
+from app.models import Base
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
---
+++ backend/alembic/versions/e407ce43f4d8_add_users_table.py
@@ -0,0 +1,35 @@
+"""add users table
+
+Revision ID: e407ce43f4d8
+Revises: initial
+Create Date: 2026-04-14 11:24:44.097060
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'e407ce43f4d8'
+down_revision: Union[str, Sequence[str], None] = 'initial'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    op.create_table(
+        'users',
+        sa.Column('id', sa.Integer(), primary_key=True, autoincrement=True),
+        sa.Column('email', sa.String(), unique=True, index=True, nullable=False),
+        sa.Column('hashed_password', sa.String(), nullable=False),
+        sa.Column('full_name', sa.String(), nullable=True),
+        sa.Column('disabled', sa.Boolean(), default=False)
+    )
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    op.drop_table('users')
